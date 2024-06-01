3752d3ec32623840adb2e88e7e4aff03a11ffe0f
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-06-01 23:29:51 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+# unordered , changeble , and Do not Allow Duplicate Values
+
+dict = {
+  "Name" :"Faiz",
+  "Age" : 19,
+  "Roll No." : 8, 
+  "DOB" : "12/02/2005" 
+}
+print (dict,"\n")
+print ( "Size of Dictionary = ",len(dict))
