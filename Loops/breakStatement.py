fbc7a05919dc8e9f718f25e1ae37a4b8a25ede78
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-06-01 23:28:38 +0530

    Add files via upload

---
+++ Loops/breakStatement.py
@@ -0,0 +1,5 @@
+number = [1,2,3,4,5,67,89,100]
+for x in number :
+    print(x)
+    if x == 67:
+        break
---
+++ Loops/forLoop.py
@@ -0,0 +1,8 @@
+number = [1,2,3,4,5,67,89,100]
+for x in number :
+    print(x)
+    
+    
+y = "Fruit"
+for z in y :
+    print (z.upper())
